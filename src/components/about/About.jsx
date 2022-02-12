2e33930ccbe7b50589b7c3dfb5a16807ced31fba
MietoProgramming <kamdox7@gmail.com>
2022-02-12 21:51:04 +0100

    About view animations

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,27 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/About.scss";
 
 const About = () => {
+  const [dialogOneShow, setDialogOneShow] = useState(false);
+  const [dialogTwoShow, setDialogTwoShow] = useState(false);
+  const [dialogThreeShow, setDialogThreeShow] = useState(false);
+
+  const [showIcons, setShowIcons] = useState(true);
+
+  const handleUnclick = () => {
+    [
+      [dialogOneShow, setDialogOneShow],
+      [dialogTwoShow, setDialogTwoShow],
+      [dialogThreeShow, setDialogThreeShow],
+    ].forEach((dialogPair) => {
+      const [value, action] = dialogPair;
+      if (value === true) {
+        action(!value);
+        setShowIcons(true);
+      }
+    });
+  };
+
   return (
-    <div className="aboutContainer">
-      {/* <div className="icon">
+    <div className="aboutContainer" onClick={() => handleUnclick()}>
+      <div
+        className={showIcons ? "icon" : "icon inactive"}
+        onClick={() => {
+          setDialogOneShow(!dialogOneShow);
+          setShowIcons(false);
+        }}
+      >
         <img src="/passions.png" alt="passion" />
       </div>
-      <div className="icon">
+      <div
+        className={showIcons ? "icon" : "icon inactive"}
+        onClick={() => {
+          setDialogTwoShow(!dialogTwoShow);
+          setShowIcons(false);
+        }}
+      >
         <img src="/education.png" alt="education" />
       </div>
-      <div className="icon">
+      <div
+        className={showIcons ? "icon" : "icon inactive"}
+        onClick={() => {
+          setDialogThreeShow(!dialogThreeShow);
+          setShowIcons(false);
+        }}
+      >
         <img src="/work.png" alt="work" />
-      </div> */}
-      <div className="preview">
+      </div>
+
+      <div className={dialogOneShow ? "preview active" : "preview"}>
         <div className="iconBubble">
           <img src="/passions.png" alt="passion" />
+        </div>
+        <div className="descBubble">
+          <h1 id="bubbleTitle">Passion</h1>
+          <div className="underline"></div>
+          <p id="bubbleText">
+            My passions are Street Lifting and problem solving in any version. I
+            like to try many different things. That's why I study a lot, read
+            and act. I can't imagine my life without compete with others,
+            especially with better than me. I have some achivements in Street
+            Lifting like: <br /> <br />
+            Medalist on Street Lifting Poland Championships 2021 <br />
+            Chin-Up Poland Champion cat.+85kg 2021
+          </p>
+        </div>
+      </div>
+
+      <div className={dialogTwoShow ? "preview active" : "preview"}>
+        <div className="iconBubble">
+          <img src="/education.png" alt="edication" />
         </div>
         <div className="descBubble">
           <h1 id="bubbleTitle">Education</h1>
           <div className="underline"></div>
           <p id="bubbleText">
-            My text about my education, university and those things.
+            I spend a lot of time learning new things and study from books and
+            articles. I give my best to become batter at least in one thing
+            every single day. <br />
+            How does it look...?
+            <br />
+            Let's see my GitHub Profile 😄
+          </p>
+        </div>
+      </div>
+
+      <div className={dialogThreeShow ? "preview active" : "preview"}>
+        <div className="iconBubble">
+          <img src="/work.png" alt="work" />
+        </div>
+        <div className="descBubble">
+          <h1 id="bubbleTitle">Work</h1>
+          <div className="underline"></div>
+          <p id="bubbleText">
+            Currently I try to become a React Developer. I got some experience
+            on 4 internships: <br /> <br />
+            🥟🥟🥟 in Poland and...
+            <br />
+            🍕 1 in Italy
+            <br />
+            One food - One internship 😅
           </p>
         </div>
       </div>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Contact.scss";
 
 const Contact = () => {
+  const [dialogOneShow, setDialogOneShow] = useState("dialogBox");
+  const [dialogTwoShow, setDialogTwoShow] = useState("dialogBox");
+  const [dialogThreeShow, setDialogThreeShow] = useState("dialogBox");
+
   return (
     <div className="contactContainer">
+      <div className={dialogOneShow}>
+        <p>Kamil Kałuża</p>
+      </div>
       <div
         className="icon"
         onClick={() => window.open("https://www.linkedin.com/in/kaluzakamil/")}
+        onMouseLeave={() => setDialogOneShow("dialogBox")}
+        onMouseEnter={() => setDialogOneShow("dialogBox active")}
       >
         <img src="/linkedin.png" alt="linkedin" />
+      </div>
+      <div className={dialogTwoShow}>
+        <p>MietoProgramming</p>
       </div>
       <div
         className="icon"
         onClick={() => window.open("https://github.com/MietoProgramming")}
+        onMouseLeave={() => setDialogTwoShow("dialogBox")}
+        onMouseEnter={() => setDialogTwoShow("dialogBox active")}
       >
         <img src="/github.png" alt="github" />
+      </div>
+      <div className={dialogThreeShow}>
+        <p>kamil.kaluza25@gmail.com</p>
       </div>
       <div
         className="icon"
         onClick={() => window.open("mailto:kamil.kaluza25@gmail.com")}
+        onMouseLeave={() => setDialogThreeShow("dialogBox")}
+        onMouseEnter={() => setDialogThreeShow("dialogBox active")}
       >
         <img src="/gmail.png" alt="gmail" />
       </div>
