ddf5182f5c36534c0d60c8ec359c64dc56297360
MietoProgramming <kamdox7@gmail.com>
2022-02-12 23:25:01 +0100

    home page animation slider

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,52 +1,11 @@
 import React from "react";
 import "../../styles/Home.scss";
+import SilderComp from "./SilderComp";
 
 const Home = () => {
   return (
-    <div>
-      <div className="card">
-        <div className="cardImgBoxVer">
-          <img src="/pic1.png" alt="" />
-        </div>
-        <div className="descriptionBox">
-          <p>
-            Programmer with passion not only to computers and new
-            technologies....
-          </p>
-        </div>
-      </div>
-      {/* <div className="card">
-        <div className="cardImgBoxHor">
-          <img src="/pic2.png" alt="" />
-        </div>
-        <div className="descriptionBox">
-          <p>Ready for new challanges...</p>
-        </div>
-      </div> */}
-      {/* <div className="card">
-        <div className="cardImgBox">
-          <img src="/pic3.png" alt="" />
-        </div>
-        <div className="descriptionBox">
-          <p>Always aiming high...</p>
-        </div>
-      </div>
-      <div className="card">
-        <div className="cardImgBox">
-          <img src="/pic4.png" alt="" />
-        </div>
-        <div className="descriptionBox">
-          <p>Focused on hard work....</p>
-        </div>
-      </div>
-      <div className="card">
-        <div className="cardImgBox">
-          <img src="/pic5.png" alt="" />
-        </div>
-        <div className="descriptionBox">
-          <p>To become the best version of himself.</p>
-        </div>
-      </div> */}
+    <div className="home">
+      <SilderComp />
     </div>
   );
 };
