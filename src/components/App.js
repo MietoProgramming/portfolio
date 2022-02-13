b3200ac0c42fcf0ce19ecad0651310bb2912a6f8
MietoProgramming <kamdox7@gmail.com>
2022-02-13 13:16:44 +0100

    HashRouter added, package.json update

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "../styles/App.scss";
 import Experience from "./experience/Experience";
 import Projects from "./projects/Projects";
